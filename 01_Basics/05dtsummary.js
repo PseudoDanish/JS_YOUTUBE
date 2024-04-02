70979e2afa91cc5d3e16dcc5278c02ca6a7ff64e
Danz <144316616+dRagZY2131@users.noreply.github.com>
2024-04-02 14:31:11 +0530

    learned Data Types

---
+++ 01_Basics/05dtsummary.js
@@ -0,0 +1,54 @@
+// SBN NUBS
+
+// primitive Data Types
+// String,boolean,number,null,undefined,bigint,Symbol
+
+// Reference(Non primitive Data types)
+// Arrays,Object,Functions
+
+let namee="Danish";
+let isLoggedIn=true;
+let number=7;
+let home=null;
+let life;
+let bigNum=120931283091823;
+let sign=Symbol("MD");
+
+const live=["gadha","takla","Muski"];
+let myObj={
+    name:"DanishM",
+    age:21,
+    part:"muski"
+}
+// const myFunction=function(){
+//     console.log("Hello World.")
+// }
+
+
+// =====================================================================
+ 
+// Stack(Primitive Data Types)
+// Heap(Non-Primitive Data Types)
+
+let myName="danish";
+
+let MyCurrentName=myName;
+MyCurrentName="DanMus";
+
+console.log(myName);
+console.log(MyCurrentName);
+// Stackk
+
+let myInfo={
+    email:"muski@gmail.com",
+    hobby:"Talking",
+}
+
+let myInfoTwo=myInfo;
+
+myInfoTwo.hobby="Not Talking";
+
+console.log(myInfo.hobby);
+console.log(myInfoTwo.hobby);
+// Heapp
+
---
+++ 01_Basics/06Strings.js
@@ -0,0 +1,40 @@
+const name="dragzyMuski";
+const repoCount=69;
+
+// console.log(name + "has a total of " + repoCount);
+
+// console.log(`My name is ${name} and I have a total of ${repoCount} repos on GitHub`);
+
+// const gameName=new String("dragzy");
+
+// console.log(gameName[0]);
+
+// console.log(gameName.charAt(5));
+
+// console.log(gameName.indexOf("y"));
+
+// console.log(gameName.length);
+
+// console.log(gameName.toUpperCase());
+
+// const newName=gameName.substring(0,5);
+// console.log(newName);
+
+// const anotherName=gameName.slice(-4,5);
+// console.log(anotherName);
+
+// const trimName="     HEllo     ...   Muski pagaal h ";
+
+// console.log(trimName);
+// console.log(trimName.trim()); //Trim only removes The starting and ending Spaces From The Text as seen  The output here is {HEllo     ...   Muski pagaal h}
+
+// const url="https://dragzy.com/dragonWarrior@hello.com";
+// console.log(url);
+// console.log();
+// console.log(url.replace("hello","gmail"));
+
+// console.log(url.includes("muski"));
+
+const splitName= "hello/how/are/you/doing/so/healthly/asdh/adr/asd";
+
+console.log(splitName.split("/"));
