da6a824d1412cabca993eb66e8488ac73b906887
Danz <144316616+dRagZY2131@users.noreply.github.com>
2024-04-03 15:22:33 +0530

    Update 01Arrays.js

--- 02_Basics/01Arrays.js
+++ 02_Basics/01Arrays.js
@@ -1,4 +1,31 @@
+// Different Types Of Array Representations
 
-array=[1,2,3,4,5,6]
+const myArray=[1,2,3,4,5,6]
 
-console.log(array);
+// Array Methods
+
+myArray.push(10)
+console.log(myArray);
+myArray.pop()
+
+myArray.shift()
+myArray.unshift(7)
+console.log(myArray.includes(6)); //Boolean Data Type
+console.log(myArray.indexOf(2)); //Returns Index Of The Given Value or -1 if not present
+    
+const newArray=myArray.join()
+console.log(myArray);
+console.log(newArray);
+
+        //Splice Slice Methods
+
+console.log("A)", myArray); //Original Array
+
+const mynew1=myArray.slice(1,4)
+console.log(mynew1);
+
+console.log("B)", myArray); //No changes Done With Slice()
+
+const mynew2=myArray.splice(1,4)
+console.log(mynew2);
+console.log("C)", myArray); //The output Removes The elements from The given index ranges in the Splice().
