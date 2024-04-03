4abc2e43f67a663ad7a8595ea77f39432d132958
dragzydebugs <144316616+dRagZY2131@users.noreply.github.com>
2024-04-03 17:26:31 +0530

    Objects ki kahaani p2

---
+++ 02_Basics/04Objects.js
@@ -0,0 +1,8 @@
+// object Constructor Initialisation (Singleton Objects)
+
+const tinderUser=new Object();
+tinderUser.name="Tommy";
+tinderUser.isGood=false;
+tinderUser.city="Amd";
+
+// console.log(tinderUser);
