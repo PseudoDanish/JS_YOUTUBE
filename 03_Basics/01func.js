36c15334b2c9ee85221a016381c007d2e3d87613
dragzydebugs <144316616+dRagZY2131@users.noreply.github.com>
2024-04-04 16:39:07 +0530

    Functions Parameters

--- 03_Basics/01func.js
+++ 03_Basics/01func.js
@@ -1,1 +1,32 @@
 
+function helloworld(username) {
+    if (!username) {
+        console.log("Please Enter A Username");
+        return
+    }
+    return `${username} prints out Hello World.`;
+}
+
+console.log(helloworld("Danish"));
+
+// Function Has A keyword name paranthesis and its definition where the logic is implemented and is written in {}
+//Function Parameters can be passed and they can be of various datatypes.
+
+function hellObject(object){
+    return `${object.username} has a net worth of ${object.price}.`;
+}
+
+const details = {
+    username:"Danish",
+    price:1000000000
+}
+
+console.log(hellObject(details)); 
+
+const array=[12,13,14,15,16,17,18,19,20];
+
+function arrayValues(arr1){
+    return arr1[2]
+}
+
+console.log(arrayValues(array));
