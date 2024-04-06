f13215442664e6c06b07c6fcb3ced67163a8f502
Danish Momin <144316616+pseudodanish@users.noreply.github.com>
2024-04-06 13:57:56 +0530

    Conditional Statements / Control Flow

---
+++ 04_Control/01Condition.js
@@ -0,0 +1,68 @@
+// First Type
+
+// if(true){
+//     console.log("Hello World");
+// }
+
+
+// Second Type for another instance
+// flag=false
+// if(flag){
+//     console.log("Hello World True")
+// }
+// else{
+//     console.log("Hello World False");
+// }
+
+// Third Type for another instance
+const isLoggedIn=false
+const isGuest=false
+const isNew=false
+
+// if(isLoggedIn)
+// {
+//     console.log("Welcome");
+//     if(isGuest)
+//     {
+//         console.log("Wwelcome Guet")
+//     }
+// }
+// else if(isNew)
+// {
+//     console.log("New User")
+// }
+// else{
+//     console.log("Enter Login Info")
+// }
+
+// Operators
+// <,>,=,==,!=,>=,<=,===
+
+// Fourth Instance
+const balance=1000 
+
+if(balance<500)
+{
+    console.log("Balance lower than 500");
+}
+else if(balance<700)
+{
+    console.log("Balance lower than 700");
+}
+else if(balance<900)
+{
+    console.log("Balance lower than 900");
+}
+else if(balance<1200)
+{
+    console.log("Balance lower than 1200");
+}
+else if(balance<1500)
+{
+    console.log("Balance lower than 1500");
+}
+else{
+    console.log("Balance Lower Than 5000");
+}
+
+console.log("End Of conditional Statement");
