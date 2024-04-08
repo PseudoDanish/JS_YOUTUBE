b6eb82214ea961a597dc1bbcaad3455e667fa4e6
Danish Momin <144316616+pseudodanish@users.noreply.github.com>
2024-04-08 16:51:12 +0530

    Loops Ki Kahaani

---
+++ 05_Iterations/01Loops.js
@@ -0,0 +1,41 @@
+//For Loop
+
+for (let index = 0; index < 7; index++) {
+    const element = index;
+    if(element==5)
+    {
+        console.log("5 IS The Best Num");
+    }
+    console.log(element);
+}
+
+
+// Array For Loop
+
+const myArray=["Hello","Hii","Heyy"]
+
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    console.log(element);    
+}
+
+//Break and Continues
+
+for (let index = 0; index <=25; index++) {
+    if(index==5)
+    {
+        console.log(`Detected ${index}`);
+        break
+    }
+    console.log(`Value Of I is : ${index}`);
+
+}
+for (let index = 0; index <=25; index++) {
+    if(index==5)
+    {
+        console.log(`Detected ${index}`);
+        continue
+    }
+    console.log(`Value Of I is : ${index}`);
+
+}
