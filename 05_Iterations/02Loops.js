2125b517314697c2f83afa963822959f8016050a
Danish Momin <144316616+pseudodanish@users.noreply.github.com>
2024-05-03 19:44:45 +0530

    While Do While  ki Kahaaniya

--- 05_Iterations/02Loops.js
+++ 05_Iterations/02Loops.js
@@ -1,1 +1,23 @@
 
+//  While And Do While Loops
+let index = 0;
+
+while (index <= 10) {
+    // console.log(`Value of Index is ${index}`);
+    index++;
+}
+
+let myArray = ["Ironman", "Batman", "SenMan"]
+let temp = 0;
+
+while (temp < myArray.length) {
+    // console.log(`Value iS ${temp} man`);
+    temp+=1;
+}
+
+let score=1;
+
+do{
+    console.log(`The Score is ${score} FOr The batsman 1`);
+    score+=1;
+}while(score <=19)
